f0192cdc4d18a19fd25dce18cbf70ead1bcd699d
yingjieweb <yingjieweb@qq.com>
2023-06-14 15:50:12 +0800

    feat: 🎸 add HttpCode decorator for getUserDetail interface

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,11 +3,14 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
   Param,
   Post,
   Put,
   Query,
-  Res,
+  // Res,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AddUserDto } from './dto/addUser.dto';
@@ -15,7 +18,7 @@ import { GetUserDetailDto } from './dto/getUserDetail.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
 import { DeleteUserDto } from './dto/deleteUser.dto';
 import { UserItem } from 'src/core/types/user';
-import { Response } from 'express';
+// import { Response } from 'express';
 
 @Controller('/user')
 export class UserController {
@@ -32,17 +35,30 @@ export class UserController {
   }
 
   @Get('getUserDetail/:id')
+  @HttpCode(200)
   getUserDetail(
     @Param('id', GetUserDetailDto) id: string,
-    @Res() res: Response,
-  ): void {
+  ): UserItem | HttpException {
     const targetUser = this.userService.getUserDetail(id);
     if (targetUser) {
-      res.status(200).json(targetUser);
+      return targetUser;
     } else {
-      res.status(404).json({ message: 'User was not found' });
+      throw new HttpException('User was not found', HttpStatus.NOT_FOUND);
     }
   }
+
+  // @Get('getUserDetail/:id')
+  // getUserDetail(
+  //   @Param('id', GetUserDetailDto) id: string,
+  //   @Res() res: Response,
+  // ): void {
+  //   const targetUser = this.userService.getUserDetail(id);
+  //   if (targetUser) {
+  //     res.status(200).json(targetUser);
+  //   } else {
+  //     res.status(404).json({ message: 'User was not found' });
+  //   }
+  // }
 
   @Put('updateUser')
   updateUser(@Body() userData: UpdateUserDto): UserItem[] {
