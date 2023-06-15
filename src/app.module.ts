3972f428269e14560f82e119765dcaf09a09b5c6
yingjieweb <yingjieweb@qq.com>
2023-06-15 11:18:14 +0800

    feat: 🎸 import mongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://yourServer:port', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
