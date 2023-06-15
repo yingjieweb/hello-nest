dae9df1c58be76da887bf10657f75f926e288106
yingjieweb <yingjieweb@qq.com>
2023-06-15 11:20:47 +0800

    feat: 🎸 create user schema

---
+++ src/modules/user/schema/user.schema.ts
@@ -0,0 +1,21 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  id: number;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  age: number;
+
+  @Prop({ required: true })
+  gender: 1 | 2;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
