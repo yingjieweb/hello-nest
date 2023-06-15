948f9c88e3638e6dce401034e81eafabbc81a600
yingjieweb <yingjieweb@qq.com>
2023-06-15 11:21:26 +0800

    feat: 🎸 import user schema in user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema, collection: 'user' },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
