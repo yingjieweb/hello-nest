1ee1fba76bef6ca44719fb0c4481d732de30ba35
yingjieweb <yingjieweb@qq.com>
2023-06-15 11:16:06 +0800

    docs: ✏️ update annotation

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 全局使用管道,用于controller层参数校验
+  // 全局使用管道，用于controller层参数校验
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
